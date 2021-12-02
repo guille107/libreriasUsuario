e138d1381c231e665f3dbe6479c4bb99f2b61bb5
guille107 <72316950+guille107@users.noreply.github.com>
2021-12-02 15:33:27 -0300

    2-12-21

--- Cripto/src/crTransfo/CRTransfo.java
+++ Cripto/src/crTransfo/CRTransfo.java
@@ -11,8 +11,8 @@ public class CRTransfo {
 		for(int a=0;a<textoSinEncriptar.length();a++) {
 			caracter=textoSinEncriptar.charAt(a);
 			
-			textoEncriptado=textoEncriptado+Character.toString((int)caracter+coeficiente);
-		
+			//textoEncriptado=textoEncriptado+Character.toString((int)caracter+coeficiente);
+			textoEncriptado=textoEncriptado+ (char)(int)caracter+coeficiente;
 		}
 		
 		return textoEncriptado;
@@ -20,8 +20,7 @@ public class CRTransfo {
 	}
 	
 	public static String DesEncriptaElParametro(String textoEncriptado) {
-			
-			
+						
 		char caracter;
 
 		String textoSinEncriptar="";
@@ -30,7 +29,8 @@ public class CRTransfo {
 				
 			caracter=textoEncriptado.charAt(a);
 
-			textoSinEncriptar=textoSinEncriptar+Character.toString((int)caracter-coeficiente);
+			//textoSinEncriptar=textoSinEncriptar+Character.toString((int)caracter-coeficiente);
+			textoSinEncriptar=textoSinEncriptar+(char)((int)caracter-coeficiente);
 		}
 			
 			
